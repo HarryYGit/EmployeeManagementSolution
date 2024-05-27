599aa3bd650526eca12177760b9fa561d5df6f21
Haoran Yan <84384076+HarryYGit@users.noreply.github.com>
2024-05-27 21:14:28 +1000

    structure the relationships in entity

---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace BaseLibrary.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string? CivilId { get; set; }
+        public string? FileNumber{ get; set; }
+        public string? Fullname { get; set; }
+        public string? JobName { get; set; }
+        public string? Address { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Photo { get; set; }
+        public string? Other { get; set; }
+
+
+    }
+}
---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text.Json.Serialization;
+
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+        //Relationship : One to many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
